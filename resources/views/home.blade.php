6ac12b308a8a6af0d74d6b0f6d8eb9fc925521d4
Wadhah Essam Mac <wadah-wleed@hotmail.com>
2019-05-11 22:43:27 +0300

    setting up every thing
    if you want to test it with the vue components that the laravel is offering , you should register from any user account and then go to /home and you will see the cahnges

---
+++ resources/views/home.blade.php
@@ -0,0 +1,19 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div id="app">
+                    <passport-clients></passport-clients>
+                    <br/>
+                    <passport-authorized-clients></passport-authorized-clients>
+                    <br/>
+                    <passport-personal-access-tokens></passport-personal-access-tokens>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
